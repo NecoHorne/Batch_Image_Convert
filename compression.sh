295dbb782d92a48ebafdce5c86f0be895133d465
Neco Horne <necohorne@gmail.com>
2019-02-27 16:33:11 +0200

    Updated as per testing results.

--- compression.sh
+++ compression.sh
@@ -10,27 +10,26 @@
 chmod +x compression.sh
 #when script executes the target directory will be the PWD + relative path of target directory
 directory=${PWD}/$2
-#resize value can be changed
-resize=50
-#quality value changed by below value
-quality=50
-echo "target: "${directory}
-#go to directory containing the files you would like to convert
+resize=80
+quality=30
+webpQ=50
 cd $1
 #compressed files and add to your target directory
 echo "Converting .PNG files."
-magick mogrify -path ${directory} -adaptive-resize ${resize}% -quality ${quality}% *.PNG
+magick mogrify -path ${directory} -adaptive-resize ${resize}% -filter Triangle -define filter:support=2 -unsharp 0.25x0.25+8+0.065 -dither None -posterize 136 -quality ${quality}% -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace none -colorspace sRGB -strip *.PNG
 echo "Converting .jpg files"
-magick mogrify -path ${directory} -adaptive-resize ${resize}% -quality ${quality}% *.jpg
+magick mogrify -path ${directory} -adaptive-resize ${resize}% -filter Triangle -define filter:support=2 -unsharp 0.25x0.25+8+0.065 -dither None -posterize 136 -quality ${quality}% -define jpeg:fancy-upsampling=off -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace none -colorspace sRGB -strip *.jpg
 echo "Converting .JPEG files"
-magick mogrify -path ${directory} -adaptive-resize ${resize}% -quality ${quality}% *.JPEG
+magick mogrify -path ${directory} -adaptive-resize ${resize}% -filter Triangle -define filter:support=2 -unsharp 0.25x0.25+8+0.065 -dither None -posterize 136 -quality ${quality}% -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace none -colorspace sRGB -strip *.JPEG
 
+cd
+cd ${directory}
 #also create compressed webp files for all images
 echo "Creating webp files from PNG's"
-magick mogrify -format WEBP -path ${directory} -quality ${quality}% -define webp:lossless=true *.PNG
+magick mogrify -format WEBP -path ${directory} -quality ${webpQ}% -define webp:lossless=true *.PNG
 echo "Creating webp files from jpg's"
-magick mogrify -format WEBP -path ${directory} -quality ${quality}% -define webp:lossless=true *.jpg
+magick mogrify -format WEBP -path ${directory} -quality ${webpQ}% -define webp:lossless=true *.jpg
 echo "Creating webp files from JPEG's"
-magick mogrify -format WEBP -path ${directory} -quality ${quality}% -define webp:lossless=true *.JPEG
+magick mogrify -format WEBP -path ${directory} -quality ${webpQ}% -define webp:lossless=true *.JPEG
 echo "Complete"
 
